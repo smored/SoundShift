e1c0a46782a4b8364ed2062c66b1d61ad5aa99f3
GGoodacre <77134320+GGoodacre@users.noreply.github.com>
2024-03-17 17:14:52 -0700

    Working request for permissions

--- app/src/main/java/com/bcit/soundshift/MainActivity.java
+++ app/src/main/java/com/bcit/soundshift/MainActivity.java
@@ -1,11 +1,16 @@
 package com.bcit.soundshift;
 
+import static com.google.android.material.internal.ContextUtils.getActivity;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,10 +28,69 @@ import android.content.res.AssetManager;
 
 import java.io.FileOutputStream;
 import java.io.InputStream;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private Button startButton;
     private ListView shiftList;
+    private static final int PERMISSION_REQUEST_READ_MEDIA_AUDIO = 1;
+    private int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+
+
+    private MediaPlayer mediaPlayer;
+
+    private void playMusic(String filePath) {
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(MainActivity.this, Uri.parse(filePath));
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            Log.i("Debugging", "Playing music now");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void CheckPermission() {
+        // Check if the permission is not granted
+        if (checkSelfPermission(Manifest.permission.READ_MEDIA_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_MEDIA_AUDIO}, PERMISSION_REQUEST_READ_MEDIA_AUDIO);
+        } else {
+            // Permission is already granted, proceed with accessing the file
+            Log.i("PERMISSION GRANTED", "YOU ALREADY HAD FILE PERMISSION");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_READ_MEDIA_AUDIO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Log.e("ERROR", "PERMISSION DENIED");
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +99,9 @@ public class MainActivity extends AppCompatActivity {
         findView();
         makeListWork();
         setupButtonOnClickListener();
+
+
+        CheckPermission();
     }
 
     private void findView() {
@@ -47,8 +114,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // start button redirects us to the control activity
-                Intent controlActivityIntent = new Intent(MainActivity.this, ControlActivity.class);
-                startActivity(controlActivityIntent);
+                String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getPath();
+
+                String filePath = path + "/sample_music.mp3";
+                playMusic(filePath);
             }
         });
     }
