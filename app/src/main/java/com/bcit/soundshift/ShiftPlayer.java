b550ed7b09f7ccd92703dd8efa0d5e6a4ee343d5
smored <62521837+smored@users.noreply.github.com>
2024-03-18 13:10:44 -0700

    Skip functionality now works

--- app/src/main/java/com/bcit/soundshift/ShiftPlayer.java
+++ app/src/main/java/com/bcit/soundshift/ShiftPlayer.java
@@ -1,7 +1,5 @@
 package com.bcit.soundshift;
 
-
-
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -16,8 +14,7 @@ import androidx.annotation.RequiresApi;
 import java.io.IOException;
 
 public class ShiftPlayer extends MediaPlayer {
-    // TODO: Play Pause button, Find media on disk. Play song with media player. Get track length and progress. Play multiple files? Load next file while song is playing so dont have to wait
-    // Important for later: use mediaPlayer in a service so it can run as a background activity
+
     private boolean isPlaying;
     private int songLength;
     private int songProgress;
@@ -35,9 +32,10 @@ public class ShiftPlayer extends MediaPlayer {
      * @param filePath path of the song on disk
      */
     public void shift_startMusic(Context context, String filePath) throws IOException {
-        if (this.isPlaying()) {
-            Log.w("ShiftPlayer", "Trying to play but already playing");
-            this.stop();
+        boolean p = this.isPlaying();
+        if (p) {
+            Log.i("ShiftPlayer", "New track requested, switching over");
+            reset(); // Reset MediaPlayer state so we can load a new song
         }
         String path = ENV_PATH + filePath;
         Log.i("ShiftPlayer", "Attempting to play file with path: " + path);
@@ -88,6 +86,13 @@ public class ShiftPlayer extends MediaPlayer {
      */
     public boolean shift_getIsPlaying() {
         return isPlaying;
+    }
+
+    /**
+     * Loads the next song asynchronously
+     */
+    public void shift_loadNextSong() {
+        // TODO make do the thing lol
     }
 
 }
